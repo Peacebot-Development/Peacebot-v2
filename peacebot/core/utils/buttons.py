4fe29cae8fb3629c019f1409e4711b5667cad371
Nishant Sapkota <snishant306@gmail.com>
2021-11-12 17:17:10 +0545

    feat: added buttons helper function

---
+++ peacebot/core/utils/buttons.py
@@ -0,0 +1,17 @@
+import hikari
+import lightbulb
+from hikari import ButtonStyle
+from hikari.api.special_endpoints import ActionRowBuilder
+
+
+def create_source_button(
+    ctx: lightbulb.context.Context, source: str
+) -> ActionRowBuilder:
+    button = (
+        ctx.bot.rest.build_action_row()
+        .add_button(ButtonStyle.LINK, source)
+        .set_label("Source")
+        .add_to_container()
+    )
+
+    return button
