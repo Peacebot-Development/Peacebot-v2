6da8ee2aaf2f1c075bb6413ca26352b871a8041f
Nishant Sapkota <snishant306@gmail.com>
2021-11-14 22:18:09 +0545

    refactor: fixed color issue

--- peacebot/core/utils/embed_colors.py
+++ peacebot/core/utils/embed_colors.py
@@ -2,8 +2,8 @@ from enum import Enum
 
 
 class EmbedColors(Enum):
-    SUCCESS = 0x123
+    SUCCESS = 0x00FF00
     ERROR = 0xFF0000
-    GENERIC = 0x1234
-    ALERT = 0x123435
-    INFO = 0x1234
+    GENERIC = 0xFFD700
+    ALERT = 0xFFA500
+    INFO = 0x99CCFF
