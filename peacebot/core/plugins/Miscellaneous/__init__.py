eea7b3680a98499f789eb82c7b30702caeb62baa
Nishant Sapkota <snishant306@gmail.com>
2022-01-14 14:03:07 +0545

    refactor: fix version and imports for 'Miscellaneous'

--- peacebot/core/plugins/Miscellaneous/__init__.py
+++ peacebot/core/plugins/Miscellaneous/__init__.py
@@ -4,17 +4,17 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from peacebot.core.utils.time import TimeConverter
 
 
-def fetch_scheduler(ctx: lightbulb.context.Context) -> AsyncIOScheduler:
+def fetch_scheduler(ctx: lightbulb.Context) -> AsyncIOScheduler:
     return ctx.bot.d.scheduler
 
 
-async def convert_time(ctx: lightbulb.context.Context, time: str) -> float:
+async def convert_time(ctx: lightbulb.Context, time: str) -> float:
     seconds = await TimeConverter.convert(TimeConverter, ctx, time)
 
     return seconds
 
 
-async def send_remainder(ctx: lightbulb.context.Context, text: str) -> None:
+async def send_remainder(ctx: lightbulb.Context, text: str) -> None:
     await ctx.respond(
         f"{ctx.author.mention} Remainder: `{text}`",
         user_mentions=True,
