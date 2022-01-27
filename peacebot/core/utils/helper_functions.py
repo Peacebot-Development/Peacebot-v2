17b9e746015dc48c729d9bf1129dab807cdf2eb4
Nishant Sapkota <snishant306@gmail.com>
2022-01-27 15:02:10 +0545

    refactor: move 'get' function from __init__.py to helper_functions.py

--- peacebot/core/utils/helper_functions.py
+++ peacebot/core/utils/helper_functions.py
@@ -4,11 +4,15 @@ import math
 import re
 import typing as t
 from datetime import datetime, timedelta
+from operator import attrgetter
 
 import lightbulb
 from lightbulb import LightbulbError
 
 from .errors import HelpersError
+
+_ValueT = t.TypeVar("_ValueT")
+T = t.TypeVar("T")
 
 logger = logging.getLogger(__name__)
 
@@ -141,3 +145,31 @@ def timedelta_converter(unit: str):
         return timedelta(weeks=1)
     else:
         raise HelpersError("Unknown time type.")
+
+
+def get(iterable: t.Iterable[T], **attrs: t.Any) -> t.Optional[T]:
+    """A helper that returns the first element in the iterable that meets
+    all the traits passed in ``attrs``.
+    Args:
+        iterable (Iterable): An iterable to search through.
+        **attrs (Any): Keyword arguments that denote attributes to search with.
+    """
+    attrget = attrgetter
+
+    # Special case the single element call
+    if len(attrs) == 1:
+        k, v = attrs.popitem()
+        pred = attrget(k.replace("__", "."))
+        for elem in iterable:
+            if pred(elem) == v:
+                return elem
+        return None
+
+    converted = [
+        (attrget(attr.replace("__", ".")), value) for attr, value in attrs.items()
+    ]
+
+    for elem in iterable:
+        if all(pred(elem) == value for pred, value in converted):
+            return elem
+    return None
